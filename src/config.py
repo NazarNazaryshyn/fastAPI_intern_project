426253a226a6c39753010484310a233e4d8313de
NazarNazaryshyn <94219552+NazarNazaryshyn@users.noreply.github.com>
2022-10-14 15:30:31 +0300

    made some changes to redis environmental variables

--- src/config.py
+++ src/config.py
@@ -12,6 +12,9 @@ class Settings:
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+            
+    HTTP_PASSWORD: str = os.getenv("HTTP_PASSWORD")
+    HTTP_USER: str = os.getenv("HTTP_USER")
 
 
 settings = Settings()
